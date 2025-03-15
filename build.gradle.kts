8cbdb805de807d6ade6fe2aba38c524624f606ef
Glavo <zjx001202@gmail.com>
2025-03-15 14:26:16 +0800

    Bump Gradle to 8.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,10 @@
 plugins {
     id("java-library")
     id("org.glavo.compile-module-info-plugin") version "2.0"
+    id("signing")
+    id("maven-publish")
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+    id("org.glavo.load-maven-publish-properties") version "0.1.0"
 }
 
 allprojects {
@@ -25,11 +29,19 @@ allprojects {
     }
 
     group = "org.glavo"
-    version = "1.0-SNAPSHOT"
+    version = "0.1.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
     }
+}
+
+description = ""
+
+dependencies {
+    compileOnlyApi("org.jetbrains:annotations:26.0.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<JavaCompile> {
@@ -37,6 +49,42 @@ tasks.withType<JavaCompile> {
     targetCompatibility = "8"
 }
 
-dependencies {
-    compileOnlyApi("org.jetbrains:annotations:26.0.2")
+tasks.withType<GenerateModuleMetadata>().configureEach {
+    enabled = false
+}
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            version = project.version.toString()
+            artifactId = project.name
+            from(components["java"])
+
+            pom {
+                name.set(project.name)
+                description.set(project.description)
+                url.set("https://github.com/Glavo/MonetFX")
+
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("glavo")
+                        name.set("Glavo")
+                        email.set("zjx001202@gmail.com")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/Glavo/MonetFX")
+                }
+            }
+        }
+    }
 }
